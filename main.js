67ddfe1358d171aff830c32d96266259404158b3
Сергей Казарезов <serjino777@yandex.ru>
2021-07-09 15:04:40 +0300

    init_commit

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  let burgerBtn = document.getElementsByClassName('burger-menu-button')[0];
+  let nav = document.getElementsByClassName('nav')[0];
+
+  function showHideNav (nav){
+    nav.classList.toggle('nav--opened');
+    document.body.classList.toggle('scroll-disabled');
+    document.body.classList.toggle('overlay_black');
+  }
+
+  burgerBtn.addEventListener('click', function(){
+    showHideNav(nav);
+    this.classList.toggle('menu--active');
+  });
+
+});
